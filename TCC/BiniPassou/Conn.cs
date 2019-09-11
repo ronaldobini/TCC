2f06c529030402e233ce5190039bd1bdbd4ff7ff
Verkans <victor@setupnet.com.br>
2019-09-11 17:36:57 -0300

    Atualizei dnv o que o bini passo
    Tinha faltado coisa pq n vi que nao tinha baixado, tipo classe conn e mappoints

---
+++ TCC/BiniPassou/Conn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace tcc3
+{
+    public class Conn
+    {
+
+
+
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+
+
+        public MySqlConnection conectar()
+        {
+
+            server = "tccsilence.mysql.dbaas.com.br";
+            database = "tccsilence";
+            uid = "tccsilence";
+            password = "UFPRtcc2019";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            return connection;
+
+        }
+
+
+
+
+        public string conectarErros()
+        {
+            string erro = "sem erros";
+            server = "tccsilence.mysql.dbaas.com.br";
+            database = "tccsilence";
+            uid = "tccsilence";
+            password = "UFPRtcc2019";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+               
+
+
+            }
+            catch (Exception ee)
+            {
+                erro = "erro : " + ee;
+            }
+
+            return erro;
+
+        }
+
+    }
+}
