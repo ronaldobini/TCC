baba1ffccde3800ff54df07c92cc25ff00cea87e
Ronaldo Bini Jr <rbsbini@gmail.com>
2019-10-31 21:29:42 -0300

    base pra duvidas

---
+++ TCC/codBase/RelFaturamento.aspx
@@ -0,0 +1,179 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RelFaturamento.aspx.cs" Inherits="simireports.RelFaturamento" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="icon" type="image/png" href="img/syss.png">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+     <title>SimiWeb - <%=Session["swver"] %></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+</head>
+<body style="background-color:#222;" onload="onload();">
+     <div id="logo" style="margin-left:20px; float:left;">
+        <a  title="Voltar ao Inicio" href=" Relatorios.aspx"><img style=" width:50px;" src="img/syss.png" /></a>
+    </div>
+    <center>
+    <div id="titulo" style="margin-top:40px; margin-right:70px; color:white; font-size:30px;">007 - Faturamento</div>
+    <br />
+
+        <p><%=sqlview %></p>
+        <div id="filtros" style="margin-bottom:40px;">
+            <form runat="server" id="filtrosFat" action="#" method="post">
+                <table style="color:white;">
+                    <tr>
+                        <th style="width:100px;">Data Inicio<br /> (Min: 2016)</th>
+                        <th style="width:100px;">Data Fim</th>
+                        <th style="width:100px;">Empresa</th>
+                        <th style="width:100px;">Nota</th>
+                        <th style="width:130px;">Item</th>
+                        <th style="width:100px;">Nome Item</th>
+                        <th style="width:100px;">Fam. Item</th>
+                    </tr>
+                    <tr>
+                        <td style="width:140px;"><input class="form-control" style="width:120px; text-align:center;" runat="server" type="text" id="datInicio" autocomplete="off"/></td>
+                        <td style="width:140px;"><input class="form-control" style="width:120px; text-align:center;" runat="server" type="text" id="datFim"  autocomplete="off"/></td>
+                        <td style="width:120px;">
+                            <select class="form-control" style="width:100px;" id="empresa" runat="server">
+                                <option value="">Todas</option>
+                                <option value="2">02</option>
+                                <option value="3">03</option>
+                                <option value="4">04</option>
+                                <option value="5">05</option>
+                                <option value="6">06</option>
+                            </select>
+                        </td>
+                        <td style="width:150px;"><input class="form-control" style="width:130px;" runat="server" type="text" id="nota" autocomplete="off"/></td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="codItem"  autocomplete="off"/></td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="descItem"  autocomplete="off"/></td>
+                        <td style="width:140px;">
+                            <select class="form-control" style="width:120px;" id="familia" runat="server">
+                                <option value="00">Todas</option>
+                                <option value="01">SICK</option>
+                                <option value="02">LS</option>
+                                <option value="03">BRADY</option>
+                                <option value="09">BELDEN</option>
+                                <option value="34">GREENPROCESS</option>
+                                <option value="50">SERVICOS</option>
+                                <option value="98">DIVERSOS IMP</option>
+                                <option value="99">DIVERSOS</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th style="width:100px;">CNPJ Cliente</th>
+                        <th style="width:100px;">Nome Cliente</th>
+                        <th style="width:120px;">Natureza</th>
+                        <th style="width:100px;">Pedido</th>
+                        <th style="width:100px;">Pedido Cliente</th>
+                        <th style="width:100px;">Transportadora</th>
+                    </tr>
+                    <tr>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="cliente"  autocomplete="off"/></td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="nomCli"  autocomplete="off"/></td>
+                        <td style="width:120px;">
+                            <select class="form-control" style="width:100px;" id="natureza" runat="server">
+                                <option value="">Todas</option>
+                                <option value="9001">Serviço</option>
+                                <option value="1000">Produtos</option>
+                            </select>
+                        </td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="pedido"  autocomplete="off"/></td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="pedCli"  autocomplete="off"/></td>
+                        <td style="width:120px;"><input class="form-control" style="width:110px;" runat="server" type="text" id="trans"  autocomplete="off"/></td>
+                    </tr>
+                </table>
+                <br />
+                <input class="btn btn-primary btn-xs" style="background-color:#126DBD" runat="server" type="submit" value="Filtrar" onserverclick="filtrarFat_Click" />
+                <input class="btn btn-primary btn-xs" style="background-color:#0a5e03; border-color:#0a5e03;" runat="server" type="submit" value="Exportar" onserverclick="export_Click" />
+
+            </form>
+        </div>
+
+
+        <div id="resultados">
+        <div style="color:white; margin-bottom:30px;">Total Faturamentos: R$ <%= m.formatarDecimal(totFaturamento) %></div>
+            <font color=white>Mostrando <%=fats.Count%> resultados, de <%=m.configDataBanco2Human(postDatInicio)%> a <%=m.configDataBanco2Human(postDatFim)%></font><br/>
+            <table class="table table-striped table-dark" style = "max-width:95%; color:white; font-size: 12px;">
+                <%
+                    Decimal totFat = 0.0m;
+                    string totFatS = "";
+
+                    foreach (var fat in fats) {
+
+                     %> 
+                        <thead style="background-color: #070a0e; color:white;">
+                            <tr>
+                                <th style="width:100px;">Nota</th>
+                                <th style="width:100px;">Empresa</th>
+                                <th style="width:100px;">Data</th>
+                                <th style="width:100px;">CNPJ Cliente</th>
+                                <th style="width:100px;">Nome Cliente</th>
+                                <th style="width:120px;">Natureza</th>
+                                <th style="width:100px;">Pedido</th>
+                                <th style="width:100px;">Pedido Cliente</th>
+                                <th style="width:100px;">Transportadora</th>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td style="text-align:left;"><b><%= fat.Nota %></b></td>
+                            <td style="text-align:left;"><b><%= fat.Empresa %></b></td>
+                            <td style="text-align:left;"><b><%= fat.Data %></b></td>
+                            <td style="text-align:left;"><b><%= fat.ClienteCPF %></b></td>
+                            <td style="text-align:left;"><b><%= fat.NomeCliente %></b></td>
+                            <td style="text-align:left;"><b><%= fat.Natureza %></b></td>
+                            <td style="text-align:left;"><b><%= fat.Pedido %></b></td>
+                            <td style="text-align:left;"><b><%= fat.PedCli %></b></td>
+                            <td style="text-align:left;"><b><%= fat.Trans %></b></td>
+                        </tr>
+                        <tr>
+                            <td colspan ="9">
+                                <table class="table table-sm table-dark" style="background-color:#3f4142; width:100%; color:white; font-size: 12px;">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 10%;">Cod. do Item</th>
+                                            <th style="width: 35%;">Desc. Item</th>
+                                            <th style="width: 5%; text-align:center;"">Quantidade</th>
+                                            <th style="text-align:right;width: 10%;">Preço Unit</th>
+                                            <th style="text-align:right;width: 10%;">Preço Total</th>
+                                        </tr>
+                                    </thead>
+                                    <%
+                                        totFat = 0.0m;
+                                        totFatS = m.formatarDecimal(totFat);
+                                        foreach (var item in fat.Itens)
+                                        {
+                                            string codItem = item.CodItem;
+                                            string nomeItem = item.NomeItem;
+                                            int qtd = Convert.ToInt32(item.QtdAtend);
+                                            
+                                            String preUnitS = m.formatarDecimal(item.PrecoUnit);
+
+                                            totFat += item.PrecoUnit*Convert.ToInt32(item.QtdAtend);
+                                            totFatS = m.formatarDecimal(totFat);
+                                            
+                                    %>
+                                            <tr>
+                                                <td><%= codItem %></td>
+                                                <td><%= nomeItem %></td>
+                                                <td style="text-align:center;"><%= qtd %></td>
+                                                <td style="text-align:right;"><%= "R$"+preUnitS %></td>
+                                                <td style="text-align:right;"><%= "R$"+m.formatarDecimal(item.PrecoUnit*qtd) %></td>
+                                            </tr>
+                                                
+                                    <% } %>
+                                    <tr>
+                                        <td colspan="8" style="background-color: #070a0e; color:white;"><b>Total Faturamento: R$ <%= totFatS %></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                                <%
+                                    } 
+                                %>
+            </table>
+        </div>
+    
+</body>
+</html>
---
+++ TCC/codBase/RelFaturamento.aspx.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IBM.Data.Informix;
+using simireports.simireports.Classes;
+
+namespace simireports
+{
+    public partial class RelFaturamento : System.Web.UI.Page
+    {
+
+        public string postDatInicio = "";
+        public string postDatFim = "";
+        public string postUnidade = "";
+        public string postNota = "";
+        public string postItem = "";
+        public string postDesc = "";
+        public string postCliente = "";
+        public string postNomCli = "";
+        public string postNatur = "";
+        public string postPed = "";
+        public string postPedCli = "";
+        public string postTrans = "";
+        public string postFam = "";
+        public string sqlview = "";
+
+        public string getPedido = "";
+
+        public Metodos m = new Metodos();
+        public String hoje = DateTime.Today.ToString("d");
+        public string represChange = "nao";
+
+
+
+
+        public static Decimal totFaturamento = 0.0M;
+        public static String totFaturamentoS = "";
+        public String erro = " ";
+
+
+        public List<Faturamento> fats = new List<Faturamento> { };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["key"] != null)
+            {
+                //VERIFICACAO DE SESSAO E NIVEL
+                if ((int)Session["key"] <= 0)
+                {
+                    Response.Redirect("login.aspx");
+                }
+                else
+                {
+                    //VERFICA NIVEL
+                    if ((int)Session["key"] >= 1)
+                    {
+                        if ((int)Session["key"] >= 5)
+                        {
+                            represChange = "sim";
+                        }
+                    }
+                    else
+                    {
+                        erro = "Você não tem permissão para esta pagina";
+                        Response.Redirect("index.aspx");
+                    }
+                }
+
+            }
+            else
+            {
+
+                Response.Redirect("login.aspx");
+            }
+
+            //PEDIDO GET
+            getPedido = Request.QueryString["getPedido"];
+            if(getPedido != null)
+            {
+                if (getPedido.Length > 0)
+                {
+                    postPed = " AND nfi.pedido = " + getPedido + "";
+                    postDatInicio = "2016-01-01";
+                    postDatFim = hoje;
+                    postDatFim = m.configDataHuman2Banco(postDatFim);
+                    executarRelatorio();
+                }
+            }
+            
+
+        }
+
+        protected void detalhes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void filtrarFat_Click(object sender, EventArgs e)
+        {
+            
+            postDatInicio = datInicio.Value;
+            if (postDatInicio == "") postDatInicio = hoje;
+
+            postDatFim = datFim.Value;
+            if (postDatFim == "") postDatFim = hoje;
+
+            postDatInicio = m.configDataHuman2Banco(postDatInicio);
+            postDatFim = m.configDataHuman2Banco(postDatFim);
+            postUnidade = empresa.Value;
+            postNota = nota.Value;
+            if (!postNota.Equals(""))
+            {
+                postNota = " AND nota_fiscal = " + postNota + " ";
+            }
+            postItem = codItem.Value;
+            postItem = postItem.ToUpper();
+            postDesc = descItem.Value;
+            postDesc = postDesc.ToUpper();
+            postCliente = cliente.Value;
+            postCliente = postCliente.ToUpper();
+            postNomCli = nomCli.Value;
+            postNomCli = postNomCli.ToUpper();
+            postNatur = natureza.Value;
+           
+            
+            if (postNatur.Equals("1000"))
+            {
+                postNatur = " AND nfi.natureza_operacao <> 9001 ";
+            }
+            else if (postNatur.Equals(""))
+            {
+                postNatur = " AND nfi.natureza_operacao > 1000 "; //todas
+            }
+            else
+            {
+                postNatur = " AND nfi.natureza_operacao = " + postNatur + " ";
+            }
+
+            postPed = pedido.Value;
+            if (!postPed.Equals(""))
+            {
+                postPed = " AND nfi.pedido = " + postPed + "";
+            }
+
+            postFam = familia.Value;
+            if (postFam == "00")
+            {
+                postFam = "";
+            }
+            else if (postFam == "01")
+            {
+                postFam = " AND it.cod_familia = '01'";
+            }
+            else if (postFam == "02")
+            {
+                postFam = " AND it.cod_familia = '02'";
+            }
+            else if (postFam == "03")
+            {
+                postFam = " AND (it.cod_familia = '03' or it.cod_familia = '30' or it.cod_familia = '31' or " +
+                                "it.cod_familia = '32' or it.cod_familia = '33' or it.cod_familia = '97')";
+            }
+            else if (postFam == "09")
+            {
+                postFam = " AND it.cod_familia = '09'";
+            }
+            else if (postFam == "34")
+            {
+                postFam = " AND it.cod_familia = '34'";
+            }
+            else if (postFam == "50")
+            {
+                postFam = " AND it.cod_familia = '50'";
+            }
+            else if (postFam == "98")
+            {
+                postFam = " AND it.cod_familia = '98'";
+            }
+            else if (postFam == "99")
+            {
+                postFam = " AND it.cod_familia = '99'";
+            }
+            else
+            {
+                postFam = "";
+            }
+
+
+
+            postPedCli = pedCli.Value;
+            postTrans = trans.Value;
+
+            executarRelatorio();
+
+        }
+
+        protected void executarRelatorio()
+        {
+            Session["firstJ"] = "0";
+            postNota = m.configCoringas(postNota);
+            postItem = m.configCoringas(postItem);
+            postDesc = m.configCoringas(postDesc);
+            postCliente = m.configCoringas(postCliente);
+            postNomCli = m.configCoringas(postNomCli);
+
+            IfxConnection conn = new BancoLogix().abrir();
+            string sql = "select nf.dat_hor_emissao," +
+                " nfi.empresa," +
+                " nf.nota_fiscal," +
+                " nfi.item," +
+                " it.den_item_reduz," +
+                " cli.cod_cliente, " +
+                " cli.nom_cliente, " +
+                " nfi.preco_unit_liquido as valor_unit," +
+                " nfi.qtd_item," +
+                " nfi.val_bruto_item as valor_total," +
+                " nfi.natureza_operacao," +
+                " nfi.pedido," +
+                " ped.num_pedido_cli," +
+                " cli2.nom_cliente" +
+                " from fat_nf_mestre nf" +
+                "  JOIN fat_nf_item nfi ON (nf.trans_nota_fiscal = nfi.trans_nota_fiscal and nfi.empresa = nf.empresa)" +
+                "  JOIN item it ON(nfi.item = it.cod_item and nfi.empresa = it.cod_empresa)" +
+                "  JOIN pedidos ped ON(nfi.pedido = ped.num_pedido and nfi.empresa = ped.cod_empresa)" +
+                "  JOIN clientes cli on(nf.cliente = cli.cod_cliente)" +
+                "  JOIN clientes cli2 on(nf.transportadora = cli2.cod_cliente)" +
+                " where nf.dat_hor_emissao >= '" + postDatInicio + " 00:00:00' " +
+                " AND nf.dat_hor_emissao <= '" + postDatFim + " 23:59:59' " +
+                " AND nf.dat_hor_emissao >= '2016-01-01 00:00:00'" +
+                " AND nf.sit_nota_fiscal = 'N'" +
+                " AND nfi.empresa like '%" + postUnidade + "%' " +
+                " AND nfi.item like '%" + postItem + "%' " +
+                " AND it.den_item_reduz like '%" + postDesc + "%' " +
+                " AND cli.cod_cliente like '%" + postCliente + "%' " +
+                " AND cli.nom_cliente like '%" + postNomCli + "%' " +
+                " " + postNota + " " +
+                " " + postNatur + " " +
+                " " + postPed + " " +
+                " " + postFam + " " +
+                " AND ped.num_pedido_cli like '%" + postPedCli + "%' " +
+                " AND nf.transportadora like '%" + postTrans + "%' " +
+                " ORDER BY nota_fiscal ";
+
+            //sqlview = sql; //ativa a exibicao do sql na tela
+            IfxDataReader reader = new BancoLogix().consultar(sql, conn);
+
+            totFaturamento = 0.0M;
+
+            DateTime dat = new DateTime();
+            string empresa = "";
+            string nota = "";
+            string codItem = "";
+            string descItem = "";
+            string cpfCli = "";
+            string cliente = "";
+            string preUnitS = "";
+            int qtdItem = 0;
+            string totalS = "";
+            string nat = "";
+            string pedido = "";
+            string pedCli = "";
+            string trans = "";
+
+            List<Item> itens = new List<Item>();
+            Item item = null;
+            Faturamento fat = null;
+            bool primeiro = true;
+            string notAnt = "";
+            string errosql = new BancoLogix().consultarErros(sql,conn);
+
+            if (reader != null && reader.HasRows)
+            {
+                string resultLog = Metodos.inserirLog((int)Session["idd"], "Executou Rel Faturamento", (string)Session["nome"], postDatInicio + " | " + postCliente);
+                while (reader.Read())
+                {
+
+                    nota = reader.GetString(2);
+
+                    if (primeiro)
+                    {
+                        notAnt = reader.GetString(2);
+                        primeiro = false;
+                    }
+                    else
+                    {
+                        if (nota.Equals(notAnt))
+                        {
+                            itens.Add(item);
+                        }
+                        else
+                        {
+                            itens.Add(item);
+                            fat = new Faturamento(dat, empresa, notAnt, cpfCli, cliente, itens, nat, pedido, pedCli, trans);
+                            fats.Add(fat);
+                            itens = new List<Item>();
+                            notAnt = nota;
+                        }
+                    }
+                    dat = new DateTime();
+                    dat = reader.GetDateTime(0);
+                    empresa = reader.GetString(1);
+                    codItem = reader.GetString(3);
+                    descItem = reader.GetString(4);
+                    cpfCli = reader.GetString(5);
+                    cliente = reader.GetString(6);
+                    preUnitS = reader.GetString(7);
+                    preUnitS = m.pontoPorVirgula(preUnitS);
+                    Decimal preUnit = Decimal.Round(Decimal.Parse(preUnitS), 2);
+                    qtdItem = m.qtdLogixToInt(reader.GetString(8));
+                    //qtdItem = m.pontoPorVirgula(qtdItem);
+                    //qtdItem = Convert.ToString(Decimal.Round(Decimal.Parse(qtdItem), 0));
+                    totalS = reader.GetString(9);
+                    totalS = m.pontoPorVirgula(totalS);
+                    Decimal total = Decimal.Round(Decimal.Parse(totalS), 2);
+                    nat = reader.GetString(10);
+                    pedido = reader.GetString(11);
+                    pedCli = reader.GetString(12);
+                    trans = reader.GetString(13);
+                    //item = new Item(qtdItem, descItem, codItem, preUnit);
+                    item = new Item(codItem,0,0,qtdItem,descItem,preUnit,"",0,0,0,0,0,0);
+
+                    totFaturamento += total;
+                }
+                totFaturamentoS = m.formatarDecimal(totFaturamento);
+
+                itens.Add(item);
+                fat = new Faturamento(dat,empresa, nota, cpfCli,cliente, itens, nat, pedido, pedCli, trans);
+                fats.Add(fat);
+            }
+            else
+            {
+                String erro = "null";
+                try
+                {
+                    erro = new BancoLogix().abrirErros();
+                    if (!erro.Equals("sem erros"))
+                    {
+                        Comissao comissao = new Comissao("NULL", erro, "-", "-", "-", 0, 0, 0, "-", new DateTime(), new DateTime(), new DateTime(), 'T', "-");
+                        //comissoes.Add(comissao);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    erro = "---" + ex;
+                    Comissao comissao = new Comissao("NULL", erro, "-", "-", "-", 0, 0, 0, "-", new DateTime(), new DateTime(), new DateTime(), 'T', "-");
+                    //comissoes.Add(comissao);
+                }
+
+            }
+            new BancoLogix().fechar(conn);
+        }
+
+
+
+        protected void export_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RelFaturamentosEx.aspx");
+        }
+
+
+
+
+
+
+
+
+    }
+}
