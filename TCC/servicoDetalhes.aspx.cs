4597cda6ed38cf37ed78da8bb664f619ad0a3870
Ronaldo Bini Jr <rbsbini@gmail.com>
2019-11-09 17:56:12 -0300

    tela servico-chat

---
+++ TCC/servicoDetalhes.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TCC.Classes;
+
+namespace TCC
+{
+    public partial class servicoDetalhes : System.Web.UI.Page
+    {
+
+        public int servicoget = 1;
+        public string nomeEmpresa = " - ";
+        public string nomeCliente = " - ";
+        public double valor = " A definir ";
+        public datetime dataEstimada  = " A definir ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Servico serv = new ServicoDAO().selectServico(servicoget);
+            Empresa emp = new EmpresaDAO().selectEmp(serv.IdEmp);
+            Usuario usu = new UsuarioDAO().selectUser(serv.IdUser);
+            List<ServicoTecnico> empServ = new ServicoTecnicoDAO().selectServTecsServ(serv.Id);
+
+
+            nomeEmpresa = emp.RazaoSocial;
+            nomeCliente = usu.Nome;
+            valor = serv.Valor;
+            dataEstimada = serv.DataFimEst;
+
+
+
+
+        }
+    }
+}
