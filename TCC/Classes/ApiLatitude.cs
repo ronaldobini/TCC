d00b44ad2982a52b8d342624beccd1001cadff69
Ronaldo Bini Jr <rbsbini@gmail.com>
2019-11-17 20:15:23 -0300

    API

--- TCC/Classes/ApiLatitude.cs
+++ TCC/Classes/ApiLatitude.cs
@@ -10,12 +10,10 @@ namespace TCC.Classes
 
 
 
-        public void clicou(object sender, EventArgs e)
+        public double pegatLat(string busca)
         {
-
-            endereco = txEndereco.Value;
             key = "AIzaSyCSbnmsy0j1KVVLOG8FxGELMeha_pqnvYo";
-            url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + endereco + "&key=" + key;
+            url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + busca + "&key=" + key;
 
 
             WebClient wc = new WebClient();
@@ -31,8 +29,30 @@ namespace TCC.Classes
 
             string endBonito = vetor[0]["formatted_address"].ToString();
 
-            mostrar = endBonito + " - Coo: " + lat + " " + lng;
+            return lat;
+        }
 
+
+        public double pegarLng(string busca)
+        {
+            key = "AIzaSyCSbnmsy0j1KVVLOG8FxGELMeha_pqnvYo";
+            url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + busca + "&key=" + key;
+
+
+            WebClient wc = new WebClient();
+            var json = wc.DownloadString(url);
+
+            var objeto = JObject.Parse(json); // parse as array  
+
+            JArray vetor = (JArray)objeto["results"];
+
+
+            double lat = double.Parse(vetor[0]["geometry"]["location"]["lat"].ToString());
+            double lng = double.Parse(vetor[0]["geometry"]["location"]["lng"].ToString());
+
+            string endBonito = vetor[0]["formatted_address"].ToString();
+
+            return lng;
 
         }
 
