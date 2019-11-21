d81d117e993e9ae7e0a38ec763900319d7b67e8a
Ronaldo Bini Jr <rbsbini@gmail.com>
2019-11-21 11:10:34 -0300

    fix servicos FIN

--- TCC/Classes/ServicoDAO.cs
+++ TCC/Classes/ServicoDAO.cs
@@ -329,23 +329,7 @@ namespace TCC.Classes
         }
         
         
-        public List<Servico> selectAllServsEmpFin(int idEmp)
-        {
-            string sql = "SELECT * FROM servico WHERE id_empresa = " + idEmp + " AND sit = 4";
-            List<Servico> servs = new List<Servico>();
-            Servico serv = new Servico();
-            MySqlDataReader reader = null;
-            MySqlConnection conn = new Conn().conectar();
-            reader = new Conn().consultar(sql, conn);
-
-            while (reader.Read() && reader.HasRows)
-            {
-                serv = preencherServ(reader);
-                servs.Add(serv);
-            }
-            conn.Close();
-            return servs;
-        }
+     
 
         public List<Servico> selectAllServsEmpSolic(int idEmp)
         {
